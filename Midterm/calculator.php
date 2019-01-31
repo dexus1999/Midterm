8962ab65baa14897b639c313f70d314b76578a1b
dexus1999 <46857766+dexus1999@users.noreply.github.com>
2019-01-31 16:37:35 +0800

    Add files via upload

---
+++ Midterm/calculator.php
@@ -0,0 +1,50 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Calculator</title>
+		
+	</head>
+<body>
+	<form name="=form1" method="get">
+
+	</form>
+	<table border="1">
+			<tr>
+				<td ></td>
+			</tr>
+			
+			<tr>
+				<td>
+					<input value="7" type="button" onclick="document.getElementById("display")/>
+					<input value="8" type="button" onclick="document.getElementById("display")/>
+					<input value="9" type="button" onclick="document.getElementById("display")/>
+					<input value="/" type="button" onclick="document.getElementById("display")/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input value="6" type="button" onclick="document.getElementById("display")/>
+					<input value="5" type="button" onclick="document.getElementById("display")/>
+					<input value="4" type="button" onclick="document.getElementById("display")/>
+					<input value="*" type="button" onclick="document.getElementById("display")/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input value="1" type="button" onclick="document.getElementById("display")/>
+					<input value="2" type="button" onclick="document.getElementById("display")/>
+					<input value="3" type="button" onclick="document.getElementById("display")/>
+					<input value="-" type="button" onclick="document.getElementById("display")/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input value="0" type="button" onclick="document.getElementById("display")/>
+					<input value="." type="button" onclick="document.getElementById("display")/>
+					<input value="+" type="button" onclick="document.getElementById("display")/>
+					<input value="=" type="button" onclick="document.getElementById("display")/>
+				</td>
+			</tr>
+		</table>
+</body>
+</html>
